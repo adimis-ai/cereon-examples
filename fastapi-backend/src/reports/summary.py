dfff4f057db2775e4d6928dc8e3983f084c58cd7
Aditya Mishra <adimis.ai.001@gmail.com>
2025-11-27 16:08:21 +0530

    update summary.py

--- fastapi-backend/src/reports/summary.py
+++ fastapi-backend/src/reports/summary.py
@@ -1,1 +1,12 @@
 # fastapi-backend/src/reports/summary.py
+
+from cereon_sdk.fastapi import (
+    BaseCard,
+    ChartCardRecord,
+    TableCardRecord,
+    NumberCardRecord,
+)
+
+from fastapi import FastAPI
+from typing import List, AsyncIterable
+from datetime import datetime, timedelta
